f2210f6c303be79fb2032937697fbe98e9ca0e40
homeversum <homeversum.eu@gmail.com>
2023-08-07 05:43:56 +0200

    initial commit

---
+++ src/main/java/eu/homeversum/testLab/TestLabApplication.java
@@ -0,0 +1,13 @@
+package eu.homeversum.testLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/homeversum/testLab/TestLabApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.homeversum.testLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
