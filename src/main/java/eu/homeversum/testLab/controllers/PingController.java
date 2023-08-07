6d7ed23dd03b84e5efe571dd3b68ede5af007654
homeversum <homeversum.eu@gmail.com>
2023-08-07 06:04:33 +0200

    Add class Ping.java

---
+++ src/main/java/eu/homeversum/testLab/controllers/PingController.java
@@ -0,0 +1,15 @@
+package eu.homeversum.testLab.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Pong!";
+    }
+
+
+}
